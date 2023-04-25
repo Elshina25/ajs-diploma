3d014696ec0b73a5cfa12e5883bd67268df05f56
Анастасия Ельшина <salnaya92@gmail.com>
2023-04-25 18:03:52 +0300

    calcTileType draw border

--- src/js/utils.js
+++ src/js/utils.js
@@ -23,7 +23,23 @@
  * ```
  * */
 export function calcTileType(index, boardSize) {
-  // TODO: ваш код будет тут
+  if (index === 0) {
+    return 'top-left';
+  } else if (index === 7) {
+    return 'top-right';
+  } else if (index >= 1 && index <=6) {
+    return 'top';
+  } else if (index === 56) {
+    return 'bottom-left';
+  } else if (index === 63) {
+    return 'bottom-right';
+  } else if (index >=57 && index <= 62) {
+    return 'bottom';
+  } else if (index % 8 === 0) {
+    return 'left';
+  } else if (index % 8 === 7) {
+    return 'right';
+  }
   return 'center';
 }
 
